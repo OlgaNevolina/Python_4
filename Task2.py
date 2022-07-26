c7e4274102aa51a1a9524c0e9704ecf630c42918
nevol78@mail.ru <nevol78@mail.ru>
2022-07-26 23:06:54 +0300

    correct

--- Task2.py
+++ Task2.py
@@ -4,16 +4,4 @@ new_lst = []
 [new_lst.append(i) for i in lst if i not in new_lst]
 print(f"Список из неповторяющихся элементов: {new_lst}")
 
-3
-num = int(input("Введите число: "))
-i = 2 
-lst = []
 
-while i <= num:
-    if num % i == 0:
-        lst.append(i)
-        num //= i
-        i = 2
-    else:
-        i += 1
-print(f"Простые множители числа: {lst}")
